fde3774bdbdb9d2f0958dc1ab8a02b001570ce85
Kihira <zoe.kihira.lee@gmail.com>
2014-05-15 13:50:46 +0100

    Allow corruption effects to be toggled

--- src/main/java/com/kihira/corruption/Corruption.java
+++ src/main/java/com/kihira/corruption/Corruption.java
@@ -15,8 +15,12 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.FMLEventChannel;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.File;
 
 @Mod(name = "Corruption", modid = "corruption")
 public class Corruption {
@@ -26,18 +30,43 @@ public class Corruption {
 
     public static boolean isCorruptionActiveGlobal = true;
 
+    public static boolean isEnabledBlockTeleportCorr;
+    public static boolean isEnabledStoneskinCorr;
+    public static boolean isEnabledWaterAllergyCorr;
+
     public static final Logger logger = LogManager.getLogger("Corruption");
     public static final FMLEventChannel eventChannel = NetworkRegistry.INSTANCE.newEventDrivenChannel("corruption");
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
+        loadGeneralConfig(e.getSuggestedConfigurationFile());
+        registerCorruptionEffects();
+
         FMLCommonHandler.instance().bus().register(new TickHandler());
         MinecraftForge.EVENT_BUS.register(new EventHandler());
 
+        eventChannel.register(new PacketEventHandler());
+    }
+
+    private void loadGeneralConfig(File file) {
+        Configuration config = new Configuration(file);
+        Property prop;
+
+        config.load();
+
+        prop = config.get(Configuration.CATEGORY_GENERAL, "Enable Block Teleport Corruption Effect", true);
+        isEnabledBlockTeleportCorr = prop.getBoolean(true);
+        prop = config.get(Configuration.CATEGORY_GENERAL, "Enable Stone Skin Corruption Effect", true);
+        isEnabledStoneskinCorr = prop.getBoolean(true);
+        prop = config.get(Configuration.CATEGORY_GENERAL, "Enable Water Allergy Corruption Effect", true);
+        isEnabledWaterAllergyCorr = prop.getBoolean(true);
+
+        if (config.hasChanged()) config.save();
+    }
+
+    private void registerCorruptionEffects() {
         CorruptionRegistry.registerCorruptionEffect(BlockTeleportCorruption.class);
         CorruptionRegistry.registerCorruptionEffect(WaterAllergyCorruption.class);
         CorruptionRegistry.registerCorruptionEffect(StoneSkinCorruption.class);
-
-        eventChannel.register(new PacketEventHandler());
     }
 }
