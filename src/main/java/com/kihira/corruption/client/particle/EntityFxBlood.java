284194ce34caebc1f61b625e3d83af2f4a99bd53
Kihira <zoe.kihira.lee@gmail.com>
2014-05-17 22:26:53 +0100

    Update gitignore

--- src/main/java/com/kihira/corruption/client/particle/EntityFxBlood.java
+++ src/main/java/com/kihira/corruption/client/particle/EntityFxBlood.java
@@ -2,15 +2,16 @@ package com.kihira.corruption.client.particle;
 
 import net.minecraft.client.particle.EntityFX;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.MathHelper;
 
-public class EntityFxBlood extends EntityFX {
+public class EntityFXBlood extends EntityFX {
 
     private final EntityPlayer entityPlayer;
     private boolean shouldAlwaysFall;
 
-    public EntityFxBlood(EntityPlayer player) {
+    public EntityFXBlood(EntityPlayer player) {
         super(player.worldObj, player.posX + 0.1F + (player.getRNG().nextFloat() / 10F), player.boundingBox.minY + 0.5F + player.getRNG().nextFloat(), player.posZ + 0.1F + (player.getRNG().nextFloat() / 10F));
-        this.particleRed = 1F;
+        this.particleRed = MathHelper.randomFloatClamp(player.getRNG(), 0.3F, 0.8F);
         this.particleBlue = this.particleGreen = 0.1F;
         this.setSize(0.01F, 0.01F);
         this.particleGravity = 0.06F;
