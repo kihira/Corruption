fa5a89510574fa36806bb72e9dc68d503970fc13
Kihira <zoe.kihira.lee@gmail.com>
2014-05-18 11:45:04 +0100

    Refine water corruption

--- src/main/java/com/kihira/corruption/common/corruption/WaterAllergyCorruption.java
+++ src/main/java/com/kihira/corruption/common/corruption/WaterAllergyCorruption.java
@@ -1,12 +1,14 @@
 package com.kihira.corruption.common.corruption;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 import cpw.mods.fml.relauncher.Side;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.DamageSource;
 
 public class WaterAllergyCorruption extends AbstractCorruption {
+
+    private final Multiset<String> playerCount = HashMultiset.create();
 
     public WaterAllergyCorruption() {
         super("waterAllergy");
@@ -14,30 +16,29 @@ public class WaterAllergyCorruption extends AbstractCorruption {
 
     @Override
     public void init(String player, Side side) {
-        if (side == Side.SERVER) MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(player).addChatComponentMessage(new ChatComponentText("You feel your skin begin to burn slightly, perhaps from the water in the air?"));
+        if (side == Side.SERVER) {
+            this.playerCount.add(player);
+        }
     }
 
     @Override
     public void onUpdate(EntityPlayer player, Side side) {
         if (side == Side.SERVER) {
-/*            if (this.activeCount > 200) {
-                Corruption.logger.info("Removing " + this.getClass());
-                CorruptionRegistry.currentCorruption.remove(this.thePlayer, this);
-            }*/
             if (player.isInWater() || (player.worldObj.isRaining() && player.worldObj.canBlockSeeTheSky((int) player.posX, (int) player.posY, (int) player.posZ)) && player.worldObj.getTotalWorldTime() % 10 == 0) {
                 player.attackEntityFrom(DamageSource.drown, 1);
             }
+            this.playerCount.add(player.getCommandSenderName());
         }
     }
 
     @Override
     public void finish(String player, Side side) {
-
+        this.playerCount.remove(player, this.playerCount.count(player));
     }
 
     @Override
     public boolean shouldContinue(EntityPlayer player, Side side) {
-        return true;
+        return this.playerCount.count(player.getCommandSenderName()) < 1000;
     }
 
     @Override
